2ab3e5dc6a00b1fa4eceaf7ca9099da7ee15a5a8
sam <s.gelernter@gmail.com>
2021-10-12 12:04:57 -0400

    complete startup class through specs pt. 2

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -2,4 +2,47 @@ require "employee"
 
 class Startup
 
+    attr_reader :name, :funding, :salaries, :employees
+
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+    def valid_title?(title)
+        salaries.has_key?(title)
+    end
+
+    def >(startup_2)
+        self.funding > startup_2.funding
+    end
+
+    def hire(employee_name, title)
+        if !salaries.has_key?(title)
+            raise "That title is invalid" 
+        else
+            employees << Employee.new(employee_name, title)
+        end
+    end
+
+    def size
+        employees.length
+    end
+
+    def pay_employee(employee)
+        pay = salaries[employee.title]
+        if funding > pay
+            employee.pay(pay)
+            @funding -= pay
+        else
+            raise "Not enough cheddar bay biscuits, chief!"
+        end
+    end
+
+    def payday
+        employees.each { |emp| pay_employee(emp) }
+    end
+
 end
